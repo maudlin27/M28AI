6dc18ac56e95e8f44224f235b51ab8f85eae6107
maudlin27 <95254039+maudlin27@users.noreply.github.com>
2023-08-15 08:49:24 +0100

    Mod info update

--- mod_info.lua
+++ mod_info.lua
@@ -1,8 +1,8 @@
 name = "M28AI"
-uid = "fnewm028-v022-55b4-92b6-64398e7ge43f"
-version = 22
+uid = "fnewm028-v023-55b4-92b6-64398e7ge43f"
+version = 23
 copyright = "MIT License"
-description = "Competitive AI for FAF utilising advanced micro.  Faster to run than M27 but currently less challenging.  Expected to provide a challenge similar to a 600-1000 1v1-global rated player. Recent changes: Support for FA and SC campaigns (as an AI teammate). More info: https://forum.faforever.com/topic/5331/m28ai-devlog-v1"
+description = "Competitive AI for FAF utilising advanced micro.  Faster to run than M27 but slightly less challenging.  Estimated as challenging for a 600-1100 1v1-global rated player. Recent changes: Support for FA and SC campaigns (as an AI teammate). More info: https://forum.faforever.com/topic/5331/m28ai-devlog-v1"
 author = "maudlin27"
 url = ""
 icon = "/mods/M28AI/M28AI.jpg"
