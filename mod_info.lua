9944fc4d059e0390173cc7f2c5f3a0556d5ddea3
maudlin27 <wormyworms10@hotmail.com>
2024-02-06 19:55:05 +0000

    Pre-release

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "M28AI"
-uid = "fnewm028-v072-55b4-92b6-64398e7ge43f"
-version = 72
+uid = "fnewm028-v073-55b4-92b6-64398e7ge43f"
+version = 73
 copyright = "MIT License"
 description = "Competitive AI for FAF utilising advanced micro (with an Easy AI variant that disables much of this micro).  Estimated as challenging for a 700-1200 global rated player. Supports FA, SC and Seraphim campaigns (as an AI teammate and/or in place of the default campaign AI). More info: https://forum.faforever.com/topic/5331/m28ai-devlog-v1"
 author = "maudlin27"
