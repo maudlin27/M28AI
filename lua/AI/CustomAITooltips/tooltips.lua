b09f96807b92f8442bedd32d904f9bea81cafd06
maudlin27 <wormyworms10@hotmail.com>
2024-07-14 07:13:02 +0100

    Fix LOUD custom option tooltips

---
+++ lua/AI/CustomAITooltips/tooltips.lua
@@ -0,0 +1,18 @@
+Tooltips = {
+    pref_M28AIxOverwhelmR = {
+        title = "AIx Overwhelm Rate",
+        description = "Adjusts M28AIx build and cheat modifiers by this amount periodically (0.0 to disable)",
+    },
+    pref_M28AIxOverwhelmT = {
+        title = "AIx Overwhelm Interval",
+        description = "Time in minutes between each AIx overwhelm adjustment",
+    },
+    pref_M28AIxOverwhelmL = {
+        title = "AIx Overwhelm Limit",
+        description = "Stops changing the AIx overwhelm modifier once this AIx modifier is reached",
+    },
+    pref_M28TimeBetweenOrders = {
+        title = "Time between M28 orders",
+        description = "Minimum number of seconds between orders for most units (e.g. makes M28 kiting significantly weaker).  Doesnt affect certain microing",
+    },
+}
