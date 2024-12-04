f8d47757204eb5e910475146d8de8c5f2137a491
maudlin27 <wormyworms10@hotmail.com>
2024-12-04 07:48:42 +0000

    Shared armies - air staging compatibility

--- lua/AI/M28Orders.lua
+++ lua/AI/M28Orders.lua
@@ -1036,7 +1036,7 @@ function IssueTrackedRefuel(oUnit, oOrderTarget, bAddToExistingQueue, sOptionalO
 end
 
 function ReleaseStoredUnits(oUnit, bAddToExistingQueue, sOptionalOrderDesc, bOverrideMicroOrder)
-    if bDontConsiderCombinedArmy or oUnit.M28Active then
+    if bDontConsiderCombinedArmy or oUnit.M28Active or UnitHasM28ActiveCargo(oUnit) then
         --Use for air staging units
         UpdateRecordedOrders(oUnit)
         --Issue order if we arent already trying to attack them
@@ -1077,7 +1077,21 @@ function ReleaseStoredUnits(oUnit, bAddToExistingQueue, sOptionalOrderDesc, bOve
     end
 end
 
-
+function UnitHasM28ActiveCargo(oUnit)
+    --Intended for air staging buildings so they release M28 units (although will also apply to other units such as transports)
+    LOG('TEMPCODE checking if have unit with cargo')
+    if oUnit.GetCargo then
+        local tCargo = oUnit:GetCargo()
+        if M28Utilities.IsTableEmpty(tCargo) == false then
+            for iCargo, oCargo in tCargo do
+                LOG('TEMPCODE oCargo='..oCargo.UnitId..M28UnitInfo.GetUnitLifetimeCount(oCargo)..'; .M28Active='..tostring(oCargo.M28Active or false))
+                if oCargo.M28Active then
+                    return true
+                end
+            end
+        end
+    end
+end
 
 function IssueTrackedTransportUnload(oUnit, tOrderPosition, iDistanceToReissueOrder, bAddToExistingQueue, sOptionalOrderDesc, bOverrideMicroOrder)
     if bDontConsiderCombinedArmy or oUnit.M28Active then
