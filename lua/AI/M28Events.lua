11f3b2b2d7a16296f54c0f1e790d9c2516328d62
maudlin27 <wormyworms10@hotmail.com>
2025-05-23 18:48:47 +0100

    Pre-release

--- lua/AI/M28Events.lua
+++ lua/AI/M28Events.lua
@@ -90,7 +90,7 @@ end
 function OnACUKilled(oUnit)
     if M28Utilities.bM28AIInGame then
         local sFunctionRef = 'OnACUKilled'
-        local bDebugMessages = true if M28Profiler.bGlobalDebugOverride == true then   bDebugMessages = true end
+        local bDebugMessages = false if M28Profiler.bGlobalDebugOverride == true then   bDebugMessages = true end
         M28Profiler.FunctionProfiler(sFunctionRef, M28Profiler.refProfilerStart)
 
         if bDebugMessages == true then
