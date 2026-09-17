23c30cff7c60326c7cca08fdc6dbc3f6d7976711
maudlin27 <wormyworms10@hotmail.com>
2026-09-17 08:39:34 +0100

    Novax targeting tweak

--- lua/AI/M28Air.lua
+++ lua/AI/M28Air.lua
@@ -12478,14 +12478,18 @@ function GetNovaxTarget(aiBrain, oNovax)
                             iCurValue = iCurValue * 2
                         end
                     end
-                    --v317 - assume will take 1s as basic patch for revent novax nerf
-                    iTimeToKillTarget = math.max(1, (oUnit:GetHealth() + iCurShield + math.min(iMaxShield - iCurShield, iTimeToTarget * iCurDPSMod)) / math.max(0.001, iDPS - iCurDPSMod))
+                    --v317 - assume will take 1s as basic patch for revent novax nerf (v323 increased from 1s to 3s)
+                    iTimeToKillTarget = math.max(2.5, (oUnit:GetHealth() + iCurShield + math.min(iMaxShield - iCurShield, iTimeToTarget * iCurDPSMod)) / math.max(0.001, iDPS - iCurDPSMod))
                     if iMaxShield == 0 and not (EntityCategoryContains(categories.COMMAND, oUnit.UnitId)) then
                         --If target is already in firing range then prioritise low health units; if its outside firing range then instead prioritise higher health units (that are worth a detour to kill)
                         if iTimeToTarget > 0 then
                             iCurValue = iCurValue * math.max(M28UnitInfo.GetUnitHealthPercent(oUnit), 0.25)
                         elseif oUnit:GetMaxHealth() >= 600 then
                             iCurValue = iCurValue * (1 + 0.2 * (1-M28UnitInfo.GetUnitHealthPercent(oUnit)))
+                        end
+                        --v323 - further change so if target max health is low and it doesnt have a shield, then lower value so we prioritise higher total health units (while still prioritising those almost dead), with exception of volatile units like energy storage and mass fabs
+                        if oUnit:GetMaxHealth() <= 1200 and not(oUnitBP.Defense.Shield) and not(EntityCategoryContains(categories.VOLATILE, oUnit.UnitId)) then
+                            iCurValue = iCurValue * 0.5
                         end
                     end
 
